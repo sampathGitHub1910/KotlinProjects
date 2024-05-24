5c428b673b794f053e87fca07619b28cc77d413e
sampathGitHub1910 <saisampath1910@gmail.com>
2024-05-24 08:17:29 +0530

    Dashboard

--- app/src/main/java/com/example/kotlinproject02/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproject02/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.kotlinproject02
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val engineStatus: TextView = findViewById(R.id.engine_status)
+        val batteryStatus: TextView = findViewById(R.id.battery_status)
+        val tirePressure: TextView = findViewById(R.id.tire_pressure)
+
+        engineStatus.text = "Engine Status: Good"
+        batteryStatus.text = "Battery Status: Charging"
+        tirePressure.text = "Tire Pressure: Normal"
     }
 }
